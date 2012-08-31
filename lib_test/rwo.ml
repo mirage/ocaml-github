eb3183471f170f7f26403f224b9e4b6f00924f66
Anil Madhavapeddy <anil@recoil.org>
2012-08-31 15:00:36 -0700

    update RWO test case to work with new cohttp

--- lib_test/rwo.ml
+++ lib_test/rwo.ml
@@ -18,7 +18,7 @@
 open Lwt
 open Printf
 open Cohttp
-open Cohttp_lwt_client
+open Cohttp_lwt
 open Config
 
 let scopes = [Github.Scope.Public_repo] 
@@ -26,34 +26,31 @@ let step1_link = Github.URI.authorize ~scopes ~client_id ()
   
 module Resp = struct
   let wrap_html s =
-    [`String (sprintf "<html><body>%s</body></html>" s)]
+    sprintf "<html><body>%s</body></html>" s
 
   (* respond with an error *)
   let not_found req err = 
     let status = `Not_found in
-    let headers = [ "Cache-control", "no-cache" ] in
-    let resp = sprintf "<html><body><h1>Error</h1><p>%s</p></body></html>" err in
-    let body = [`String resp] in
-    Response.init ~body ~headers ~status ()
+    let headers = Header.of_list [ "Cache-control", "no-cache" ] in
+    let body = sprintf "<html><body><h1>Error</h1><p>%s</p></body></html>" err in
+    Server.respond_string ~headers ~status ~body ()
 
   (* internal error *)
   let internal_error err = 
     let status = `Internal_server_error in
-    let headers = [ "Cache-control", "no-cache" ] in
-    let resp = sprintf "<html><body><h1>Internal Server Error</h1><p>%s</p></body></html>" err in
-    let body = [`String resp] in
-    Response.init ~body ~headers ~status ()
+    let headers = Header.of_list [ "Cache-control", "no-cache" ] in
+    let body = sprintf "<html><body><h1>Internal Server Error</h1><p>%s</p></body></html>" err in
+    Server.respond_string ~headers ~status ~body
 
   (* dynamic response *)
   let dyn req body =
     let status = `OK in
-    let headers = [] in
-    Response.init ~body ~headers ~status ()
+    Server.respond_string ~body ~status ()
 
   (* index page *)
   let index req =
     let body = wrap_html (sprintf "<a href=\"%s\">step1</a>" (Uri.to_string step1_link)) in
-    return (dyn req body)
+    dyn req body
 
   (* dispatch non-file URLs *)
   let dispatch req =
@@ -62,35 +59,37 @@ module Resp = struct
     | ["index.html"]  ->
         index req
     | ["step2"] -> begin
-        let code = Request.param ~meth:`GET ~default:"" req "code" in
+        let code = try List.assoc "code" (Request.params req) with Not_found -> "" in
         try_lwt 
           lwt token = Github.Monad.(run (Github.token_of_code ~client_id ~client_secret ~code ())) in
-          return (dyn req (wrap_html ("ok: token is "^(Github.token_to_string token))))
+          dyn req (wrap_html ("ok: token is "^(Github.token_to_string token)))
         with Failure e ->
-          return (dyn req (wrap_html ("err: "^e)))
+          dyn req (wrap_html ("err: "^e))
     end
     | _ -> 
-        return (not_found req "dispatch")
+        not_found req "dispatch"
 end
 
 (* main callback function *)
-let callback con_id req =
+let callback con_id ?body req =
   let path = Request.path req in
 
-  printf "%s %s [%s]\n%!" (Common.string_of_method (Request.meth req)) path 
+  printf "%s %s [%s]\n%!" (Code.string_of_method (Request.meth req)) path 
     (String.concat "," (List.map (fun (h,v) -> sprintf "%s=%s" h v) 
-      (Request.params_get req)));
+      (Request.params req)));
 
   (* normalize path to strip out ../. and such *)
   let path_elem = Re_str.(split (regexp_string "/") path) in
-  lwt resp = Resp.dispatch req path_elem in
-  Cohttp_lwt_server.respond_with resp
+  Resp.dispatch req path_elem 
 
 let server_t =
-  let open Cohttp_lwt_server in
   let port = 8080 in
-  let spec = { default_spec with callback; port=port; auto_close=true } in
-  main spec
+  let conn_closed con_id () = () in
+  let root_dir = None in
+  let timeout = None in
+  let address = "0.0.0.0" in (* TODO address to option *)
+  let spec = { Cohttp_lwt.Server.callback; port=port; conn_closed; root_dir; timeout; address } in
+  Cohttp_lwt.Server.main spec
 
 let _ =
   Lwt_main.run server_t

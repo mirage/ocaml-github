2e5bbfd950f62b9cf706b5a6ae93a3a4352a58b1
Anil Madhavapeddy <anil@recoil.org>
2012-09-01 20:34:13 -0700

    rationalise the Cohttp/Cohttp_lwt module opens

--- lib/github.ml
+++ lib/github.ml
@@ -116,7 +116,8 @@ module Monad = struct
   let (>>=) = bind
 end
 
-module C = Cohttp_lwt
+module C = Cohttp
+module CL = Cohttp_lwt
 
 module API = struct
   open Lwt
@@ -126,7 +127,7 @@ module API = struct
     match_lwt reqfn uri with
     |None -> return (Monad.(Error No_response))
     |Some (res,body) -> begin
-      Printf.eprintf "Github response code %s\n%!" (Cohttp.Code.string_of_status (C.Response.status res));
+      Printf.eprintf "Github response code %s\n%!" (C.Code.string_of_status (CL.Response.status res));
       (* TODO check that this is a valid response code *)
       try_lwt 
         lwt r = respfn ~res ~body in
@@ -141,13 +142,13 @@ module API = struct
      |Some token -> Uri.add_query_param uri ("access_token", token) 
      |None -> uri in
     request uri req (fun ~res ~body ->
-      C.string_of_body body >>= resp
+      CL.string_of_body body >>= resp
     )
 
   (* GET wrapper that takes a URI, adds an access token and calls the 
    * result on the callback function.  *)
   let get ?headers ?token ~uri fn =
-    json_request ?token uri (C.Client.get ?headers) fn
+    json_request ?token uri (CL.Client.get ?headers) fn
 
   (* POST wrapper that takes a URI, adds an access token and calls the 
    * result on the callback function.  *)
@@ -156,14 +157,14 @@ module API = struct
     let body, clen = match body with
      |Some buf ->
        let clen = String.length buf in
-       Cohttp_lwt.body_of_string buf, clen
+       CL.body_of_string buf, clen
      |None -> None, 0 in
     (* Add the correct mime-type header *)
     let headers = match headers with
-     |Some x -> Cohttp.Header.add x "content-type" "application/json"
-     |None -> Cohttp.Header.of_list ["content-type","application/json"] in
-    let headers = Cohttp.Header.add headers "content-length" (string_of_int clen) in
-    json_request ?token uri (C.Client.post ~headers ?body) fn
+     |Some x -> C.Header.add x "content-type" "application/json"
+     |None -> C.Header.of_list ["content-type","application/json"] in
+    let headers = C.Header.add headers "content-length" (string_of_int clen) in
+    json_request ?token uri (CL.Client.post ~headers ?body) fn
 end
 
 (* Authorization request, normally not used (a link in the HTML is
@@ -172,7 +173,7 @@ let authorize ?scopes ~client_id () =
   let uri = URI.authorize ?scopes ~client_id () in
   (* Github will return a 302 usually, to the redirect_uri
    * registered in the application entry on Github *)
-    API.request uri C.Client.get (fun ~res ~body -> Lwt.return_unit)
+    API.request uri CL.Client.get (fun ~res ~body -> Lwt.return_unit)
 
 
 module Token = struct
@@ -180,7 +181,7 @@ module Token = struct
   let direct ?(scopes=[`Repo]) ~user ~pass () =
     let req = { Github_t.auth_req_scopes=scopes; auth_req_note="ocaml-github" } in
     let body = Github_j.string_of_authorization_request req in
-    let headers = Cohttp.Header.(add_authorization (init ()) (Cohttp.Auth.Basic (user,pass))) in
+    let headers = C.Header.(add_authorization (init ()) (C.Auth.Basic (user,pass))) in
     API.post ~headers ~body ~uri:URI.authorizations (fun body ->
       let open Github_t in
       let json = Github_j.authorization_response_of_string body in
@@ -192,8 +193,8 @@ module Token = struct
    *)
   let of_code ~client_id ~client_secret ~code () =
     let uri = URI.token ~client_id ~client_secret ~code () in
-    API.request uri C.Client.post (fun ~res ~body ->
-      lwt body = C.string_of_body body in
+    API.request uri CL.Client.post (fun ~res ~body ->
+      lwt body = CL.string_of_body body in
       let t = List.assoc "access_token" (Uri.query_of_encoded body) in
       Lwt.return t
     )

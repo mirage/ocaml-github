ace8a3d37e66f534684468fb2c12d4de9cd1d73d
Anil Madhavapeddy <anil@recoil.org>
2012-12-14 13:53:17 +0000

    fix test scripts for tags

--- lib_test/tags.ml
+++ lib_test/tags.ml
@@ -5,7 +5,7 @@ open Github_t
 
 let token = Config.access_token
 let get_tags_and_times ~user ~repo =
-  Github.Tag.get_tags_and_times ~user ~repo () >>=
+  Github.Tag.get_tags_and_times ~token ~user ~repo () >>=
   fun res ->
   List.iter (fun (k,v) -> eprintf "%s %s\n" k v) res;
   return ()

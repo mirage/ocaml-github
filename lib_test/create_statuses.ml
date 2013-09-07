bd62e859a7ff233f62337b1a964d3e2af329e082
Anil Madhavapeddy <anil@recoil.org>
2013-09-07 21:24:45 +0100

    suppress variable warning

--- lib_test/create_statuses.ml
+++ lib_test/create_statuses.ml
@@ -14,12 +14,12 @@ let print_statuses sl =
       s.status_created_at
       s.status_url
   ) sl
- 
+
 let t =
   let user = "dsheets" in
   let repo = "opam-repository" in
-  let sha_a = "85e86c0260dd230b6bd37c17056f8282011baf51" in
-  let sha_b = "4e89aa7f781c6f094d17079ecb6ca875327eddb8" in
+  let _sha_a = "85e86c0260dd230b6bd37c17056f8282011baf51" in
+  let _sha_b = "4e89aa7f781c6f094d17079ecb6ca875327eddb8" in
   Github.(Monad.(run (
     let status = Github_t.({
       new_status_state=`Error;

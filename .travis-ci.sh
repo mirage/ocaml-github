a68849efcb295354862e6f0fa37d7785d3d816e4
Anil Madhavapeddy <anil@recoil.org>
2014-03-01 20:26:31 +0000

    Add Travis files

---
+++ .travis-ci.sh
@@ -0,0 +1,26 @@
+OPAM_DEPENDS="lwt ssl cohttp atdgen yojson cmdliner lambda-term uri"
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam time libssl-dev
+
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+opam init
+opam pin cohttp git://github.com/avsm/ocaml-cohttp
+opam install ${OPAM_DEPENDS}
+make

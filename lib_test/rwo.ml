f76fab40a8bdfcfc434476cc95a8065ea941cced
Anil Madhavapeddy <anil@recoil.org>
2014-03-01 23:52:31 +0000

    Adapt to cohttp 0.10.x api

--- lib_test/rwo.ml
+++ lib_test/rwo.ml
@@ -74,7 +74,7 @@ module Resp = struct
 end
 
 (* main callback function *)
-let callback con_id ~body req =
+let callback con_id req body =
   let uri = Request.uri req in
   let path = Uri.path uri in
   printf "%s %s [%s]\n%!" (Code.string_of_method (Request.meth req)) path 

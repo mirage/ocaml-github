33ba36b65335214856eb7a1d86fb557eb515fd33
Anil Madhavapeddy <anil@recoil.org>
2014-03-02 18:58:19 +0000

    Pull requests are now allowed to have `null` bodys.
    They default to an empty string if null.  Closes #31

--- lib_test/pulls.ml
+++ lib_test/pulls.ml
@@ -11,9 +11,9 @@ let print_pulls pl =
   eprintf "--\n%!"
  
 let t =
-  let user = "dsheets" in
-  let repo = "opam-repository" in
-  let opam_repo_pulls = Github.Pull.for_repo ~user ~repo in
+  let user = "ocaml" in
+  let repo = "opam" in
+  let opam_repo_pulls = Github.Pull.for_repo ~user ~repo ~page:2 in
   Github.(Monad.run (opam_repo_pulls ~state:`Open ())) >|= print_pulls >>
   Github.(Monad.run (opam_repo_pulls ~state:`Closed ())) >|= print_pulls >>
   Github.(Monad.(run (
@@ -24,12 +24,12 @@ let t =
       | hd::tl ->
           Pull.get ?token ~user ~repo ~num:hd.Github_t.pull_number ()
           >>= fun p ->
-          eprintf "Inside monad: pull %d: %s\n" p.Github_t.pull_number p.Github_t.pull_title;
+          eprintf "Inside monad: pull %d: %s\n%!" p.Github_t.pull_number p.Github_t.pull_title;
           Pull.list_commits ?token ~user ~repo ~num:hd.Github_t.pull_number ()
           >>= fun commits -> List.iter (fun commit ->
             eprintf "    %s\n" commit.Github_t.commit_sha
           ) commits;
-          eprintf "---------\n";
+          eprintf "---------\n%!";
           Pull.list_files ?token ~user ~repo ~num:hd.Github_t.pull_number ()
           >>= fun files -> List.iter (fun file ->
             eprintf "    %s\n" file.Github_t.file_filename

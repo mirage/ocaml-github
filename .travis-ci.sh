c55ce6d1d0a9621d83254da0a412e9556a29632a
Anil Madhavapeddy <anil@recoil.org>
2014-04-01 15:50:59 +0100

    travis: +stringext

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt ssl cohttp atdgen yojson cmdliner lambda-term uri"
+OPAM_DEPENDS="lwt ssl cohttp atdgen yojson cmdliner lambda-term uri stringext"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;

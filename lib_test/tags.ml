876a2583344580462b255737fbb0a3fa52060c7c
David Sheets <sheets@alum.mit.edu>
2016-05-25 14:00:45 +0100

    Update the lib_test/tags test/example
    Also, try a repository that will never have tags.

--- lib_test/tags.ml
+++ lib_test/tags.ml
@@ -1,16 +1,31 @@
 open Lwt
 open Printf
-open Github.Monad
 open Github_t
 
-let token = Config.access_token
+let get_auth_token_from_jar auth_id =
+  Github_cookie_jar.init ()
+  >>= fun jar ->
+  Github_cookie_jar.get jar auth_id
+  >>= function
+  | Some auth -> return auth
+  | None -> Lwt.fail (Failure ("id '"^auth_id^"' not in cookie jar"))
+
 let get_tags_and_times ~user ~repo =
-  let stream = Github.Repo.get_tags_and_times ~token ~user ~repo () in
+  let stream = Github.Repo.get_tags_and_times ~user ~repo () in
   Github.Stream.iter (fun (k,v) ->
     eprintf "%s %s\n" k v;
-    return ()
+    Github.Monad.return ()
   ) stream
 
 ;;
-Lwt_main.run (run (get_tags_and_times ~user:"ocaml" ~repo:"opam"));
-Lwt_main.run (run (get_tags_and_times ~user:"mirage" ~repo:"ocaml-cstruct"));
+Lwt_main.run Github.Monad.(run (
+  embed (get_auth_token_from_jar "test")
+  >>= fun auth ->
+  Github.(API.set_token (Token.of_auth auth))
+  >>= fun () ->
+  get_tags_and_times ~user:"dsheets" ~repo:"axtls"
+  >>= fun () ->
+  get_tags_and_times ~user:"ocaml" ~repo:"opam"
+  >>= fun () ->
+  get_tags_and_times ~user:"mirage" ~repo:"ocaml-cstruct"
+))

067dbcbd6b202aa54e8dea0690ab383e9d00577b
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2019-06-03 11:07:01 +0200

    lib_test: add a test for the API /user/orgs

---
+++ lib_test/current_user_orgs.ml
@@ -0,0 +1,17 @@
+open Github_t
+
+let token = Config.access_token
+
+let t = Github.(Monad.(run (
+    let orgs = Organizations.current_user_orgs ~token () in
+    Stream.next orgs
+    >>= function
+    | None -> Printf.eprintf "no orgs for current user\n"; exit 1
+    | Some (first_org, _) ->
+      eprintf "org %Ld: %s\n%!" first_org.org_id first_org.org_login;
+      return ()
+)))
+
+;;
+
+Lwt_main.run t

07bdc5cd2bf40b6aa8b00ce78728699f94a02935
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2019-06-03 11:37:50 +0200

    fix current_user_orgs test

--- lib_test/current_user_orgs.ml
+++ lib_test/current_user_orgs.ml
@@ -3,12 +3,12 @@ open Github_t
 let token = Config.access_token
 
 let t = Github.(Monad.(run (
-    let orgs = Organizations.current_user_orgs ~token () in
+    let orgs = Organization.current_user_orgs ~token () in
     Stream.next orgs
     >>= function
     | None -> Printf.eprintf "no orgs for current user\n"; exit 1
     | Some (first_org, _) ->
-      eprintf "org %Ld: %s\n%!" first_org.org_id first_org.org_login;
+      Printf.eprintf "org %Ld: %s\n%!" first_org.org_id first_org.org_login;
       return ()
 )))
 
